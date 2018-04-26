14dc718a9d61f5f8eb250b584a543da483cd26d5
Shabadoo89 <supercow08@gmail.com>
2018-04-25 19:50:55 -0500

    uploaded batch of login code
    Commiting to master. This is the stuff that has the working login code.

---
+++ login system/index.php
@@ -0,0 +1,19 @@
+<?php
+    include_once 'header.php';
+?>
+
+<section class = "main-container">
+    <div class = "main-wrapper">
+        <h2>Home</h2>
+        
+        <?php
+            if (isset($_SESSION['u_id']))
+                echo "You are logged in!";
+        ?>
+
+    </div>
+</section>
+
+<?php
+    include_once 'footer.php';
+?>
